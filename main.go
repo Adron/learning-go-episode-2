53eaa2ff4f8f6686c3ddcb18f1029fac45b59f76
Adron Hall <adron@thrashingcode.com>
2019-01-19 12:04:11 -0800

    adding temp package.

--- main.go
+++ main.go
@@ -1,32 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type Celsius float64
-type Fahrenheit float64
-
-const (
-	AbsoluteZeroC Celsius = -273.15
-	FreezingC Celsius = 0
-	BoilingC Celsius = 100
+	tempconv "github.com/Adron/awesomeProject/tempconv"
 )
 
-func CelsiusToFahrenheit(c Celsius) Fahrenheit {
-	return Fahrenheit(c*9/5 + 32)
-}
-
-func FahrenheitToCelsius(f Fahrenheit) Celsius {
-	return Celsius((f-32) * 5/9)
-}
-
-func main () {
+func main() {
 	fmt.Println("Temperatures are hard to figure off the top of one's mind!")
 
-	boilingF := CelsiusToFahrenheit(BoilingC)
+	boilingF := tempconv.CelsiusToFahrenheit(BoilingC)
 
 	fmt.Printf("%v\n", boilingF)
 	fmt.Printf("%s\n", boilingF)
 	fmt.Println(boilingF)
 	fmt.Printf("%g\n", boilingF)
-
 }
